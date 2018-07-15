6ae309082b723d6081b1eca4267d0f6e3a79eb53
capitalch <capitalch@gmail.com>
2018-07-15 19:11:01 +0530

    stable

--- patch/Track+ remoteinsert.sql
+++ patch/Track+ remoteinsert.sql
@@ -8,6 +8,8 @@ begin
 --GST applicable in India w.e.f 1st July 2017
 --Taken care of GST rates 18% and 28%
   declare @date date;
+  declare @acc_id unsigned integer;
+  declare @acc_id_cash unsigned integer;
   declare @gst18 unsigned integer;
 
   set @date = "date"(@srec_date);
@@ -18,20 +20,32 @@ begin
   end if;
   if @rectype = 'Y' then
     set @rectype = 'S'
-  else set @rectype = 'R'
+  else 
+    set @rectype = 'R'
   end if;
+
+  select acc_id into @acc_id from acc_main where acc_code = 'custadvance' and acc_root = 'Y';
+  select default_cash_id into @acc_id_cash from acc_setup;
 
   if @ptype = 'I' then //insert
     select acc_id into @gst18 from acc_main where acc_code = 'SaleGST18%';
     if @gst18 is not null then
-        insert into bill_memo( type,bill_memo,ref_no,"date",sale_tax_sale_id,
-          total_amt,acc_id,descr,mpreserve, cgst, sgst, igst, gstin ) 
-          values( @rectype,'M',@rec_no,@date,@gst18,@rec_amt,(select default_cash_id
-            from acc_setup),'Job No:'+@job_no + ':Invoice:' + rtrim(@other_info),'N', @cgst, @sgst, @igst, @gstin );
-    end if;
-    
+        if @isAdvance = 'y' then
+            insert into cash_receipt (acc_id, acc_id_cash, rec_date,ref_no, remarks, amt)
+                values (@acc_id, @acc_id_cash, @date, @rec_no, 'Job No:'+@job_no+ ' Advance receipt from service+', @rec_amt );
+        else
+            insert into bill_memo( type,bill_memo,ref_no,"date",sale_tax_sale_id,
+              total_amt,acc_id,descr,mpreserve, cgst, sgst, igst, gstin ) 
+              values( @rectype,'M',@rec_no,@date,@gst18,@rec_amt,@acc_id_cash,
+                'Job No:'+@job_no + ':Invoice:' + rtrim(@other_info),'N', @cgst, @sgst, @igst, @gstin );
+        end if;
+     end if;
   else
-    delete from bill_memo where ref_no = @rec_no
+    //if @isAdvance = 'y' then
+        delete from cash_receipt where ref_no = @rec_no;
+    //else
+        delete from bill_memo where ref_no = @rec_no;
+    //end if
   end if
 end
 go
